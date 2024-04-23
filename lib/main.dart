90b6dcfe62689b5f7f1e0387f1a1f9e92a5c89fc
hagaryasser25 <75675516+hagaryasser25@users.noreply.github.com>
2024-04-23 11:13:19 +0200

    fourth

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:courses_app/screens/home_page.dart';
+import 'package:courses_app/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomePage(),
+      home: SplashScreen(),
     );
   }
 }
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,73 @@
+import 'package:courses_app/screens/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      builder: (context, child) => Scaffold(
+          body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          children: [
+            Align(alignment: Alignment.topRight, child: InkWell(
+              onTap: (){
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const HomePage()),
+                  );
+              },
+              child: Text('skip'))),
+            Image.asset('assets/images/logo.png'),
+            SizedBox(
+              height: 20.h,
+            ),
+            Image.asset(
+              'assets/images/splash.jpg',
+              height: 300.h,
+            ),
+            SizedBox(
+              height: 20.h,
+            ),
+            Image.asset('assets/images/text.jpg'),
+            SizedBox(
+              height: 10.h,
+            ),
+            Align(
+                alignment: Alignment.center,
+                child: Text(
+                    'We have brought together amazing skillset you to learn at any convenient time.',
+                    style: TextStyle(color: HexColor('#B4B4B4')))),
+            SizedBox(
+              height: 20.h,
+            ),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: HexColor('#3E7BF4'),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const HomePage()),
+                  );
+                },
+                child: Text(
+                  'Get Started',
+                  style: TextStyle(color: Colors.white),
+                ))
+          ],
+        ),
+      )),
+    );
+  }
+}
